f90f98f931d672bae4ff5df8cc5afa5536dd9c3c
Diego Ferreira <devdiegoferreira@gmail.com>
2022-10-07 17:30:16 -0300

    style: add body and root styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,4 +11,15 @@ export default createGlobalStyle`
     cursor: pointer;
     border: none;
   }
+
+  body {
+    background-color: ${({ theme }) => theme.darkBlue};
+  }
+
+  #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
 `;
