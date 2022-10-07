6cbca0c731e9b6f8e328f776514b9ee1180591f2
Diego Ferreira <devdiegoferreira@gmail.com>
2022-10-07 17:14:51 -0300

    chore: add Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,15 @@
+import { CardContainer } from './style';
+
+const Card = () => {
+  return (
+    <CardContainer>
+      <span>advice #000</span>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque,
+        aperiam.
+      </p>
+    </CardContainer>
+  );
+};
+
+export default Card;
---
+++ src/components/Card/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.section`
+  width: 100%;
+  max-width: 33.75rem;
+  min-height: 20.75rem;
+`;
