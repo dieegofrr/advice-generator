5ed7c51fb4da13409de699195b421bdde5312bea
Diego Ferreira <devdiegoferreira@gmail.com>
2022-10-08 16:27:37 -0300

    feat: add labels to Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,11 +4,21 @@ import { CardContainer } from './style';
 
 const Card = () => {
   const { advice, getAdvice } = useAdvice();
+  const { id, advice: text } = advice;
 
   return (
     <CardContainer>
-      <span>advice #{advice.id}</span>
-      <p>{advice.advice}</p>
+      <section className="data">
+        <span tabIndex={0} aria-label="advice number {id}">
+          {id ? `advice #${id}` : 'advice #000'}
+        </span>
+        <p tabIndex={0}>{text ? text : 'wait a sec'}</p>
+      </section>
+      <img
+        src="/imgs/pattern-divider-mobile.svg"
+        alt="divider"
+        className="divider"
+      />
       <Button onClick={getAdvice} />
     </CardContainer>
   );
