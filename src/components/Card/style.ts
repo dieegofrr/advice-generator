77c352a8a8319e314baba098a03eb36799bc89fe
Diego Ferreira <devdiegoferreira@gmail.com>
2022-10-08 15:17:32 -0300

    style: add Card style

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const CardContainer = styled.section`
   background-color: ${({ theme }) => theme.darkGrayishBlue};
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
   max-width: 33.75rem;
   min-height: 20.75rem;
