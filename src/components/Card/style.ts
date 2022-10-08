81af28ac42fd1c53fa8408537453b24f9acd2951
Diego Ferreira <devdiegoferreira@gmail.com>
2022-10-08 16:28:10 -0300

    style: add style to Card

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -4,22 +4,49 @@ export const CardContainer = styled.section`
   background-color: ${({ theme }) => theme.darkGrayishBlue};
   position: relative;
   display: flex;
-  flex-direction: column;
   align-items: center;
+  flex-direction: column;
   width: 100%;
   max-width: 33.75rem;
   min-height: 20.75rem;
   border-radius: 0.875rem;
   padding: 3.25rem 3rem;
 
-  span {
-    color: ${({ theme }) => theme.neonGreen};
-    text-transform: uppercase;
+  section.data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2.25rem;
+    padding-bottom: 2.25rem;
+
+    span {
+      color: ${({ theme }) => theme.neonGreen};
+      font-size: 1.25rem;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+    }
+
+    p {
+      color: ${({ theme }) => theme.lightCyan};
+      font-size: clamp(
+        1rem,
+        2.5vw + 0.1rem,
+        ${({ theme }) => theme.defaultFontSize}
+      );
+      font-weight: ${({ theme }) => theme.defaultFontWeight};
+      text-align: center;
+
+      &::before {
+        content: '“';
+      }
+
+      &::after {
+        content: '”';
+      }
+    }
   }
 
-  p {
-    color: ${({ theme }) => theme.lightCyan};
-    font-size: ${({ theme }) => theme.defaultFontSize};
-    font-weight: ${({ theme }) => theme.defaultFontWeight};
+  img.divider {
+    width: 100%;
   }
 `;
