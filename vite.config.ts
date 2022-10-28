96df965327271d5dfe585b1f752254fad4d6df7c
Diego Ferreira <devdiegoferreira@gmail.com>
2022-10-28 15:32:05 -0300

    fix: new vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 import { defineConfig } from 'vite';
-import path from 'node:path';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
