766f248de9c78d1edb7cc71523ba173121e6834a
Diego Ferreira <devdiegoferreira@gmail.com>
2022-10-28 13:43:05 -0300

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import path from 'node:path';
 import react from '@vitejs/plugin-react';
@@ -5,6 +7,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
